10ab5bd74c2871ef85d895462a0413e6caa2df18
Mauro Marssola <marssolinha@hotmail.com>
2017-07-05 22:58:11 -0300

    Nodes edit

--- cpp/serial/nodes.cpp
+++ cpp/serial/nodes.cpp
@@ -14,14 +14,17 @@ void Nodes::addNodes(QString get_serial)
 
     for (const QString &str: read_list) {
         QString node = QString(str.mid(5, 4));
-        QString status = QString(str.mid(1, 4));
+        QString status = QString(str.mid(4, 1));
+        QString range = QString(str.mid(1, 3));
 
         QJsonObject new_object;
         new_object.insert("status", status);
+        new_object.insert("range", range);
 
         QJsonObject search = nodes_object[node].toObject();
         if (!search.isEmpty()) {
-            if (nodes_object[node].toObject().value("status").toString() != status) {
+            if (nodes_object[node].toObject().value("status").toString() != status ||
+                    nodes_object[node].toObject().value("range").toString() != range) {
                 nodes_object[node] = new_object;
                 nodes_update.append(node);
             }
@@ -30,7 +33,6 @@ void Nodes::addNodes(QString get_serial)
             nodes_update.append(node);
         }
     }
-
     getNodesChanged();
 }
 
@@ -58,6 +60,6 @@ void Nodes::requireGetAllNodes()
     nodes_object_send.insert("Nodes", nodes_object);
     nodes_array.push_back(nodes_object_send);
 
-    qDebug() << nodes_array;
+    //qDebug() << nodes_array;
     emit sendAllNodes(nodes_array);
 }
